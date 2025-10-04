d62b016d88e6274ea5ae143a384a660ffc70edbc
Hector Selby Reimundez <hectordavid.selby@gmail.com>
2025-10-04 15:04:58 +0100

    insta setting added to velocity

--- badg/phys/velo.lua
+++ badg/phys/velo.lua
@@ -5,12 +5,13 @@ setmetatable(VeloProp, {
     
     __call = function(self, p)
 
-        assert(p.accel and p.decel and p.max)
+        assert((p.insta or p.accel) and p.decel and p.max)
 
         local v = {
 
             accel = p.accel,
             decel = p.decel,
+            insta = p.insta,
             max = p.max,
 
             min = p.min or 0,
@@ -30,11 +31,13 @@ function accel(p)
 
     assert(p.velo and p.dt and p.veloProp)
 
-    if p.velo < p.veloProp.max then
-        return min(p.velo + p.veloProp.accel * p.dt, p.veloProp.max) end
-
     if p.velo > p.veloProp.max and p.veloProp.maintain then
-        return max(p.velo - p.veloProp.maintain * p.dt, p.veloProp.max) end
+        local decel = p.veloProp.maintain or p.veloProp.decel
+        return max(p.velo - decel * p.dt, p.veloProp.max) 
+    end
+
+    if p.velo < p.veloProp.max and not p.veloProp.insta then
+        return min(p.velo + p.veloProp.accel * p.dt, p.veloProp.max) end
 
     return p.veloProp.max
 end
@@ -52,5 +55,6 @@ function rever(p)
     assert(p.velo and p.dt and p.veloProp)
 
     if p.velo == p.veloProp.min then return p.veloProp.min end
-    return max(p.velo - (p.veloProp.decel + p.veloProp.accel) * p.dt, p.veloProp.min)
+    if p.veloProp.insta and abs(p.velo) < p.veloProp.max then return p.veloProp.min end
+    return max(p.velo - (p.veloProp.decel + (p.veloProp.accel or 0)) * p.dt, p.veloProp.min)
 end
